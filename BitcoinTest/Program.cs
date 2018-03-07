6230256e06910246ecb8f2f7cdab8dfbc0756d53
Erik Rodhe <erik.rodhe@bambora.com>
2018-03-07 13:28:10 +0100

    Intial commit

---
+++ BitcoinTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BitcoinTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
